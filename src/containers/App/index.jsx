2c083911a869347ee0db99588921fbb7c375e2c5
microlws <microlws@gmail.com>
2018-02-04 16:45:01 +0100

    fixed trailing spaces

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -4,7 +4,7 @@ import './index.scss';
 
 const App = () => (
   <div className='App'>
-    add new components or HTML here by importing them and then applying as a JSX tag  
+    add new components or HTML here by importing them and then applying as a JSX tag
   </div>
 );
 
